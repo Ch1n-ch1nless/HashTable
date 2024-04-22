a132a17023b1cad113d24a222d22f832e45ad9b8
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-04-23 02:08:59 +0300

    Change graphics

--- HashTable/source/hash_search.s
+++ HashTable/source/hash_search.s
@@ -1,2 +1,20 @@
-global FastHashSearch
+global FastHashTableSearch
 
+section .text
+
+;===============================================
+;Fast assembler implementation of HashTableSearch
+;Entry: RDI =   address of hash table
+;       RSI =   address of string 'key'
+;       RDX =   len of string 'key'
+;Exit:  RAX =   1, if elem is in hash table, else 0
+;Dstr:  RCX, RBX
+;===============================================
+FastHashTableSearch:
+;v~~~Calculate FastHashCrc32~~~v
+            mov rcx, rdx                        ; RCX = len of cycle
+            mov rax, 0xFFFFFFFF                 ; EAX = (uint32_t)-1
+
+
+
+            crc32 
